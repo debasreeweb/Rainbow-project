14521d0ba39d528022dc27565d180bbf4fc48bfb
Debasreearoy <106072480+Debasreearoy@users.noreply.github.com>
2022-05-26 12:32:50 +0100

    Create calculater.php
    This is form created by HTML

---
+++ calculater.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="testdata.php" method='get'> 
+   firstNumber:<input type="number" name="number1" id="name"> 
+   <br><br>
+   secondNumber:<input type="number" name="number2" id="lastname">
+   <br><br>
+   <button>submit</button>
+
+   </form>
+</body>
+</html>
