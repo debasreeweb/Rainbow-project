8ac921877b39dd618e13ee1a2d438c0a6f3664da
Debasreearoy <106072480+Debasreearoy@users.noreply.github.com>
2022-05-26 13:28:38 +0100

    Add files via upload

---
+++ testdata.php
@@ -0,0 +1,17 @@
+<?php
+echo "<pre>";
+ print_r($_GET);
+ echo "</pre>";
+
+ echo "<br>";
+ echo $_GET['name'].'  ';
+ echo $_GET['lastname'];
+
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+// echo $_POST['name'];
+
+?>
